66106d38a62aa1563902025cc5511237316bec64
Robert Edmonds <edmonds@fsi.io>
2014-01-22 14:29:57 -0500

    setup.py: only skip the cython rebuild if dnstable.c is available

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ VERSION = '0.2'
 
 from distutils.core import setup
 from distutils.extension import Extension
+import os
 
 try:
     from Cython.Distutils import build_ext
@@ -15,8 +16,11 @@ try:
         cmdclass = {'build_ext': build_ext},
     )
 except ImportError:
-    setup(
-        name = NAME,
-        version = VERSION,
-        ext_modules = [ Extension('dnstable', ['dnstable.c'], libraries = ['dnstable']) ],
-    )
+    if os.path.isfile('dnstable.c'):
+        setup(
+            name = NAME,
+            version = VERSION,
+            ext_modules = [ Extension('dnstable', ['dnstable.c'], libraries = ['dnstable']) ],
+        )
+    else:
+        raise
